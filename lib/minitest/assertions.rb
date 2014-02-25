0d7721620db8afe142f273e810f2f3da7741cf29
John Bohn <jjbohn@gmail.com>
2014-02-25 09:57:37 -0600

    Reuse assert|refute_valid in assert|refute_validates_presence_of

--- lib/minitest/assertions.rb
+++ lib/minitest/assertions.rb
@@ -1,45 +1,45 @@
 module MiniTest::Assertions
   def assert_validates_presence_of(object, property)
-    assert_valid(object)
+    assert_valid object
     tmp = object.send("#{property}")
     object.send("#{property}=", nil)
-    refute object.valid?, "Expected object not to be valid"
-    assert object.errors[property.to_sym], "Expected errors to exist for #{property}"
+    refute_valid object
+    refute_nil object.errors[property.to_sym], "Expected errors to exist for #{property}"
     object.send("#{property}=", tmp)
   end
 
   def refute_validates_presence_of(object, property)
-    assert_valid(object)
+    assert_valid object
     tmp = object.send("#{property}")
     object.send("#{property}=", nil)
-    assert_valid(object)
+    assert_valid object
     assert_nil object.errors[property.to_sym]
     object.send("#{property}=", tmp)
   end
 
   def assert_validates_attachment_presence_of(object, property)
-    assert_valid(object)
+    assert_valid object
     tmp = object.send("#{property}_file_name")
     object.send("#{property}_file_name=", nil)
-    refute object.valid?, "Expected object not to be valid"
-    assert object.errors[property.to_sym], "Expected errors to exist for #{property}"
+    refute_valid object
+    refute_nil object.errors[property.to_sym], "Expected errors to exist for #{property}"
     object.send("#{property}_file_name=", tmp)
   end
 
   def refute_validates_attachment_presence_of(object, property)
-    assert_valid(object)
+    assert_valid object
     tmp = object.send("#{property}_file_name")
     object.send("#{property}_file_name=", nil)
-    assert_valid(object)
+    assert_valid object
     assert_nil object.errors[property.to_sym]
     object.send("#{property}_file_name=", tmp)
   end
 
-  def assert_valid(object)
+  def assert_valid object
     assert object.valid?, "Expected object to be valid"
   end
 
-  def refute_valid(object)
+  def refute_valid object
     refute object.valid?, "Expected object to not be valid"
   end
 end
